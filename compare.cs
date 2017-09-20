2bdd3160602d066bdd389b138b162380e0b190fc
Saidmamad <saidmamad@gmail.com>
2017-09-20 17:36:49 +0530

    Update1 of Compare-The-Triplets

--- compare.cs
+++ compare.cs
@@ -1,54 +1,37 @@
-/* Is there any other way to solve this problem?
-*  It just compares two scores, the scores of person A, who has 3 marks, a0, a1, a2, b0, b1, b2.  
-*  If the score of a0 > b0, it should give to student A one point, and so on.
-*/
-
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 class Solution {
-   
-    
+
+
+    static int[] getPoint(int a, int b, ref int scoreA, ref int scoreB) {
+        if (isValidParam(a, b))
+            {
+                if(a>b){
+                   scoreA+=1;
+                }
+                else if(b>a){
+                    scoreB+=1;
+                }
+            
+            } else {
+                Console.WriteLine("The score value should be between 0 and 100");
+            }
+        int[] rez = {scoreA,scoreB};
+        
+        return rez;
+    }
+
     static int[] solve(int a0, int a1, int a2, int b0, int b1, int b2){
-        int scoreA = 0;
-        int scoreB = 0;
-        
-        if(CheckParam(a0,b0)){
-            if(a0>b0){
-               scoreA+=1;
-            }
-            else if(b0>a0){
-                scoreB+=1;
-            }
-            
-        }else {
-            Console.WriteLine("The score value should be between 0 and 100");
-        }
-        
-        if(CheckParam(a1,b1)){
-            if(a1>b1){
-               scoreA+=1;
-            }
-            else if(b1>a1){
-                scoreB+=1;
-            }
-            
-        }else {
-            Console.WriteLine("The score value should be between 0 and 100");
-        }
-        
-        if(CheckParam(a2,b2)){
-            if(a2>b2){
-               scoreA+=1;
-            }
-            else if(b2>a2){
-                scoreB+=1;
-            }
-            
-        }else {
-            Console.WriteLine("The score value should be between 0 and 100");
-        }
+        public static int scoreA = 0;
+        public static int scoreB = 0;
+
+        int[] result = new int[0];
+        result[] += getPoint(a0, b0, ref scoreA, ref scoreB);
+        result[] += getPoint(a1, b1, ref scoreA, ref scoreB);
+        result[] += getPoint(a1, b1, ref scoreA, ref scoreB);
+
         
         int[] result = {scoreA,scoreB};
         
@@ -56,15 +39,10 @@ class Solution {
     }
 
     
-    static bool CheckParam(int x, int y){
-        if(x>=0 && x<=100 && y>=0 && y<=100)
-        { 
-            return true; 
-        }
-        else
-        {
-            return false;
-        }
+    static bool isValidParam(int x, int y){
+
+        return (x >= 0 && x <= 100 && y >= 0 && y <= 100);
+        
     }
 
     
@@ -80,8 +58,9 @@ class Solution {
         int b2 = Convert.ToInt32(tokens_b0[2]);
         int[] result = solve(a0, a1, a2, b0, b1, b2);
         Console.WriteLine(String.Join(" ", result));
+
+        Console.ReadLine();
     }
 
 }
-
 
